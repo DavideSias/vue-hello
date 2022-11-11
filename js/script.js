de36da8615d1ad54d057d4b796001dd52a5fd533
DavideSias <davidesias77@gmail.com>
2022-11-11 14:32:51 +0100

    First commit, add h1 and img

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    
+    el: '#root',
+    data: {
+        text: 'Ciao, io sono un h1 creato con Vue.js',
+        imgLink: 'https://picsum.photos/200/300',
+        imgAlt: 'immagine di lorem picsum',
+    }
+});
